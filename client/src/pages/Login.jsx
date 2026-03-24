347dd4ff986a0a86123e375aa5eb51b8b33df8a3
Vidit Kumar Singh <viditkumar69@gmail.com>
2026-03-25 01:16:40 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://fairchance-backend.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
