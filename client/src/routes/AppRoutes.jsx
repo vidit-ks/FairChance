f862e81a7142a785ee115cc5fdb108437e5c991c
Vidit Kumar Singh <viditkumar69@gmail.com>
2026-03-25 12:10:25 +0530

    Update AppRoutes.jsx

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,7 +6,7 @@ import Dashboard from "../pages/Dashboard";
 import ProtectedRoute from "./ProtectedRoute";
 import Charities from "../pages/Charities";
 import Admin from "../pages/Admin";
-//import ProtectedRoute from "./ProtectedRoute";
+import AdminRoute from "./AdminRoute";
 
 function AppRoutes() {
   return (
@@ -16,9 +16,6 @@ function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/charities" element={<Charities />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-<Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
 
         <Route
           path="/dashboard"
@@ -26,6 +23,15 @@ function AppRoutes() {
             <ProtectedRoute>
               <Dashboard />
             </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <AdminRoute>
+              <Admin />
+            </AdminRoute>
           }
         />
       </Routes>
