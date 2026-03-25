b6b9be00254f79a88437b59a8e2055cdd700a4cd
Vidit Kumar Singh <viditkumar69@gmail.com>
2026-03-26 01:19:54 +0530

    Update authRoutes.js

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,8 +1,10 @@
+import { getAllUsers } from "../controllers/authController.js";
 const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/authController");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/users", getAllUsers);
 
 module.exports = router;
